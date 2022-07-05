0e78575b589e63866eea488b7c4f4d5794acf79e
dkenw <dkenw1618@gmail.com>
2022-07-05 22:42:52 +0800

    Simplify useChainId

--- src/hooks/useChainId.ts
+++ src/hooks/useChainId.ts
@@ -1,14 +1,6 @@
-import { useNetwork } from 'wagmi'
+import { useProvider } from 'wagmi'
 
 export const useChainId = () => {
-  const { chain } = useNetwork()
-
-  if (chain) return chain?.id
-
-  const chainId = (window?.ethereum as any)?.chainId as string | undefined
-
-  if (chainId != null && `${chainId}`.startsWith('0x')) {
-    return parseInt(`${chainId}`.slice(2)) || undefined
-  }
-  return undefined
+  const provider = useProvider()
+  return provider.network.chainId
 }
